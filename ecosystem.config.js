76082b6ff85b4f1ca180179d3db7567cfec5d4ea
Zach Peters <zpeters@gmail.com>
2022-04-08 18:49:51 -0500

    updats

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'api',
-      namespace: 'serve',
       script: 'node_modules/.bin/rw',
       args: 'serve api',
       instances: 'max',
       exec_mode: 'cluster',
-      wait_ready: false,
+      wait_ready: true,
       listen_timeout: 10000,
     },
   ],
